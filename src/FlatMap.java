f49bc17efb5f77085b812176ab5c2f7c92c5b99a
sadjad.hoodgar <sadjad.hoodgar@adevinta.com>
2022-07-24 01:26:00 +0200

    main class added.

---
+++ src/FlatMap.java
@@ -0,0 +1,34 @@
+
+import java.util.AbstractMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public final class FlatMap {
+
+    public static Map<String, Object> flatten(Map<String, Object> map) {
+        return map.entrySet().stream()
+                .flatMap(FlatMap::flatten)
+                .collect(LinkedHashMap::new, (m, e) -> m.put("/" + e.getKey(), e.getValue()), LinkedHashMap::putAll);
+    }
+    private static Stream<Map.Entry<String, Object>> flatten(Map.Entry<String, Object> entry) {
+        if (entry == null) {
+            return Stream.empty();
+        }
+
+        if (entry.getValue() instanceof Map<?, ?>) {
+            return ((Map<?, ?>) entry.getValue()).entrySet().stream()
+                    .flatMap(e -> flatten(new AbstractMap.SimpleEntry<>(entry.getKey() + "/" + e.getKey(), e.getValue())));
+        }
+
+        if (entry.getValue() instanceof List<?>) {
+            List<?> list = (List<?>) entry.getValue();
+            return IntStream.range(0, list.size())
+                    .mapToObj(i -> new AbstractMap.SimpleEntry<String, Object>(entry.getKey() + "/" + i, list.get(i)))
+                    .flatMap(FlatMap::flatten);
+        }
+        return Stream.of(entry);
+    }
+}
